76fe4798a9e8bfe43eb7b083e023c47295546558
redpanda-cat <leungs1@mskcc.org>
2020-11-13 18:08:41 -0500

    Add Colossus module

--- alhena/alhena_data.py
+++ alhena/alhena_data.py
@@ -5,6 +5,9 @@ logger = logging.getLogger('alhena_loading')
 from scgenome.db.qc import cache_qc_results
 import alhena.constants as constants
 
+
+__BASE_URL = "https://colossus.canadacentral.cloudapp.azure.com/api"
+
 def download_analysis(dashboard_id, data_directory, sample_id, library_id, description):
     directory = os.path.join(data_directory, dashboard_id)
 
@@ -21,13 +24,25 @@ def download_analysis(dashboard_id, data_directory, sample_id, library_id, descr
 
 
 def create_analysis_metadata(dashboard_id, directory, sample_id, library_id, description):
-    metadata = {
-        "dashboard_id": dashboard_id,
-        "sample_id": sample_id,
-        "library_id": library_id,
-        "description": description
-    }
+    user = os.environ['COLOSSUS_API_USERNAME']
+    password = os.environ['COLOSSUS_API_PASSWORD']
+
+
+    response = requests.get(
+        constants.COLOSSUS_BASE_URL + '/analysis_information/?analysis_jira_ticket=' + jira_id, auth=(user, password))
+
+    data = response.json()['results'][0]
+    metadata = get_metadata_record(data, dashboard_id)
 
     logger.info("Creating metadata file")
     with open(os.path.join(directory, constants.METADATA_FILENAME), 'w+') as outfile:
         json.dump(metadata, outfile)
+
+
+def get_metadata_record(analysis, dashboard_id):
+    return {
+        "sample_id": analysis["library"]["sample"]["sample_id"],
+        "library_id": analysis["library"]["pool_id"],
+        "dashboard_id": dashboard_id,
+        "description": analysis["library"]["description"]
+    }
--- alhena/constants.py
+++ alhena/constants.py
@@ -2,3 +2,5 @@
 DASHBOARD_ENTRY_INDEX = "analyses"
 DATA_TYPES = ["qc", "segs", "bins", "gc_bias"]
 METADATA_FILENAME = "metadata.json"
+
+COLOSSUS_BASE_URL = "https://colossus.canadacentral.cloudapp.azure.com/api"
